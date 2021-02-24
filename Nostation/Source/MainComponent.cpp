17bcbecc209c4b40ae39f8a711e72f41a502f65c
Richard Simchik <rts2904@rit.edu>
2021-02-24 14:02:58 -0500

    Create simple audio application
    Includes the main audio IO loop which outputs a 440Hz sine wave. Also
    creates a blank window and outputs text to the console when debugging.

---
+++ Nostation/Source/MainComponent.cpp
@@ -0,0 +1,83 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+
+    DBG("Hello, World!");
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+
+    this->sampleRate = sampleRate;
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+    float** audioIOChannels = bufferToFill.buffer->getArrayOfWritePointers();
+	for (int sample = 0; sample < bufferToFill.numSamples; sample++)
+	{
+		for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); channel++)
+		{
+            audioIOChannels[channel][sample] = std::sin(sinePhase) * 0.5;
+		}
+        sinePhase += juce::MathConstants<float>::twoPi * (440.0 / sampleRate);
+        if (sinePhase >= juce::MathConstants<float>::twoPi) sinePhase -= juce::MathConstants<float>::twoPi;
+	}
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
